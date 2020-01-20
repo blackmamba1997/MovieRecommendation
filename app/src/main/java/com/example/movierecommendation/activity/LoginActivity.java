d632872edca6d173cbf687bdaec37b40e61c4500
blackmamba1997 <36701947+blackmamba1997@users.noreply.github.com>
2020-01-20 23:47:44 +0530

    Added LoginActivity signIn feature

--- app/src/main/java/com/example/movierecommendation/activity/LoginActivity.java
+++ app/src/main/java/com/example/movierecommendation/activity/LoginActivity.java
@@ -5,15 +5,19 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.movierecommendation.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,38 +27,50 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText usernameEditText;
     EditText passwordEditText;
-    Button loginButton ;
-    ProgressBar loadingProgressBar ;
+    Button loginButton;
+    ProgressBar loadingProgressBar;
     FirebaseAuth mAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth= FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         usernameEditText = findViewById(R.id.username);
-        passwordEditText= findViewById(R.id.password);
-        loginButton=findViewById(R.id.login);
-        loadingProgressBar= findViewById(R.id.loading);
-        final String username=usernameEditText.getText().toString();
-        final String password=usernameEditText.getText().toString();
-        boolean correctuser= Pattern.matches("[a-zA-Z0-9._]@[a-zA-Z][.]com",username);
-        boolean correctpass= Pattern.matches("[a-zA-Z0-9._#]",password);
-        if(correctuser && correctpass){
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+        loadingProgressBar = findViewById(R.id.loading);
 
-            mAuth.signInWithEmailAndPassword(username,password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(//task successful){
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                            }else{
+                String username = usernameEditText.getText().toString();
+                String password = usernameEditText.getText().toString();
+                boolean correctuser = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE).matcher(username).matches();
+                boolean correctpass = true;//Pattern.matches("[a-zA-Z0-9._#]+", password);
 
-                            }
-                        }
-                    });
-        }
+                if (correctpass && correctuser) {
 
+                    mAuth.signInWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(LoginActivity.this, user.getEmail(), Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "oops", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                } else {
+                    Toast.makeText(LoginActivity.this, "enter valid credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
 
 
     }
